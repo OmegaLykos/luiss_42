8ab7b6f8ce89b3b54b8a67089adab0b516b1f3c6
Luigi Di Bella <ldi-bell@e3r10p3.42roma.it>
2023-09-21 17:17:07 +0200

    primo backup

---
+++ test_c00/scopes.c
@@ -0,0 +1,29 @@
+
+int function_1(int variable_1, int variable_2)
+{
+    int variable_3;
+
+	variable_3 = variable_1 + variable_2;
+	return (variable_3);
+}
+
+
+
+/*
+    THE PROGRAM ALWAYS STARTS WITH MAIN FUCTION
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	b = 5;
+	a = function_1(b, 2);
+
+	return (0);
+}
+
+
+
+
+
