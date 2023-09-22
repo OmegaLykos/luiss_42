61f090bbb689c796a5fe53e564f502d966439a40
Luigi Di Bella <ldi-bell@e3r10p3.42roma.it>
2023-09-22 11:21:01 +0200

    Aggiornamento c00

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldi-bell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 10:15:03 by ldi-bell          #+#    #+#             */
+/*   Updated: 2023/09/22 10:30:01 by ldi-bell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+		write(1,&c,1);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+		int		i;
+		i = 122;
+		while(i >= 97)
+		{
+				ft_putchar(i);
+				i--;
+		}
+}
+/*
+int main(void)
+{
+		ft_print_reverse_alphabet();
+		return 0;
+}
+*/
