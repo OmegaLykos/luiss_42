3bb1ff9f096f51d9715069c5aa4b144c91b0d631
Luigi Di Bella <ldi-bell@e3r1p6.42roma.it>
2023-10-02 17:01:18 +0200

    aggiornato c03

---
+++ projects/c03/ex05/ft_strlcat.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldi-bell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 16:56:17 by ldi-bell          #+#    #+#             */
+/*   Updated: 2023/10/02 16:59:46 by ldi-bell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	res_d;
+	unsigned int	res_s;
+
+	i = ft_strlen(dest);
+	j = 0;
+	res_d = ft_strlen(dest);
+	res_s = ft_strlen(src);
+	if (size < 1)
+		return (res_s + size);
+	while (src[j] && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	if (size < res_d)
+		return (res_s + size);
+	else
+		return (res_d + res_s);
+}
+
+/*int				main(void)
+{
+	char			dest[50] = "sefsfesa";
+	char			src[50] = "";
+	unsigned int	size;
+
+	size = 0;
+	printf("%lu\n", strlcat(dest, src, size));
+//	printf("%d\n", ft_strlcat(dest, src, size));
+	printf("dest: %s\nsrc: %s\n", dest, src);
+}*/
