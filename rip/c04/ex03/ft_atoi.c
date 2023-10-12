9718b305d2a83bf159630da678fb986b029d3cef
Luigi Di Bella <ldi-bell@e3r1p5.42roma.it>
2023-10-12 15:12:16 +0200

    backup 1012 1512

--- rip/c04/ex03/ft_atoi.c
+++ rip/c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ldi-bell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 09:51:05 by ldi-bell          #+#    #+#             */
-/*   Updated: 2023/10/12 11:20:30 by ldi-bell         ###   ########.fr       */
+/*   Updated: 2023/10/12 14:01:09 by ldi-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,29 @@ int ft_atoi(char *str)
 	i = 0;
 	result = 0;
 	sign = 1;
-	while (str[i] == 
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
+
+/*
+#include <stdio.h>
+   
+int main(void)
+{
+	char *s = "   ---+--+01234506789ab567";
+	printf("%d", ft_atoi(s));
+}
+*/
